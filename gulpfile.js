87d13eb6c0cf50a70ef110d41736510108b2f91f
Tim Frankie <tfrankie88@gmail.com>
2018-05-16 09:19:50 -0400

    add build task

--- gulpfile.js
+++ gulpfile.js
@@ -73,7 +73,7 @@ gulp.task('default', ['clean'], function () {
 });
 
 // Push build to gh-pages
-gulp.task('deploy', function () {
+gulp.task('deploy', ['build'], function () {
   return gulp.src("./dist/**/*")
     .pipe(deploy())
 });
